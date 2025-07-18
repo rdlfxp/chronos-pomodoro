63f1e62974446b7a02dee8c5acab0c176b952716
Rodolfo Nossa <rdlfxp@gmail.com>
2025-07-18 14:07:34 -0300

    Finalização do componente Heading e tipagem TypeScript de children (ReactNode)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import './styles/theme.css';
+import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
-function App() {
-  const [count, setCount] = useState(0);
-
+export function App() {
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
+      <Heading attr={123} attr2='String'>
+      <Heading>
+        Olá mundo 1
+        <button>
+          <TimerIcon />
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>
-        Click on the Vite and React logos to learn more
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
+        incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
+        laudantium voluptas praesentium labore dolore unde! Aliquam, dolorem! A,
+        quia rerum!
       </p>
     </>
   );
 }
-
-export default App;
