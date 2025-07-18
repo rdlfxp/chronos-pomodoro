489c60e0c2e6e25d1d7c5c9717fc0aa664e576dc
Rodolfo Nossa <rdlfxp@gmail.com>
2025-07-18 20:21:16 -0300

    Atualização index

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { Heading } from './components/Heading.tsx';
+import './styles/theme.css';
+import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
+
+export function App() {
+  return (
+    <>
+      <Heading>
+        Olá mundo 1
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum cumque
+        incidunt accusamus commodi quasi sit, doloribus in fugiat, repellat
+        laudantium voluptas praesentium labore dolore unde! Aliquam, dolorem! A,
+        quia rerum!
+      </p>
+    </>
+  );
+}
